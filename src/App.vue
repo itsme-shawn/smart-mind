68a4d7dbef177c0bcde418ecabb5cf02733a2f34
Keep-Going-HyeonSoo <chs98105@gmail.com>
2020-10-03 02:48:20 +0000

    메뉴바(좌측 네비게이션) 수정

--- src/App.vue
+++ src/App.vue
@@ -13,28 +13,21 @@
       <v-btn icon to="/about">
         <v-icon>mdi-magnify</v-icon>
       </v-btn>
+			
+			<v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
     </v-app-bar>
 		
 		<v-navigation-drawer app v-model="drawer">
-			<v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="title">
-            Application
-          </v-list-item-title>
-          <v-list-item-subtitle>
-            subtext
-          </v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-
-      <v-divider></v-divider>
+			
 			
 			<site-menu></site-menu>
       
 		</v-navigation-drawer>
-		<v-content>
+		<v-main>
 			<router-view/>
-		</v-content>
+		</v-main>
 		<site-footer :footer="myFooter"></site-footer>
   </v-app>
 </template>
--- src/views/site/menu.vue
+++ src/views/site/menu.vue
@@ -3,19 +3,50 @@
         dense
         nav
       >
-        <v-list-item
-          v-for="item in items"
-          :key="item.title"
-          link
-        >
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
+		<v-list-item>
+			<v-list-item-content>
+				<v-list-item-title class="title">Dashboard</v-list-item-title>
+				<v-list-item-subtitle>정신차렷!</v-list-item-subtitle>
+			</v-list-item-content>
+		</v-list-item>
 
+		<v-divider></v-divider>
+	
+		<v-list>
+			
+			<v-list-item>
+        <v-list-item-icon>
+          <v-icon>mdi-home</v-icon>
+        </v-list-item-icon>
+
+      <v-list-item-title>홈</v-list-item-title>
+				
+				
+      </v-list-item>
+      <v-list-group
+        v-for="(item, i) in items"
+        :key="i"
+        v-model="item.active"
+        :prepend-icon="item.icon"
+        no-action
+      >
+        <template v-slot:activator>
           <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
+            <v-list-item-title v-text="item.title"></v-list-item-title>
+          </v-list-item-content>
+        </template>
+
+        <v-list-item
+          v-for="subItem in item.subItems"
+          :key="subItem.title"
+					:to="subItem.to"
+        >
+          <v-list-item-content>
+            <v-list-item-title v-text="subItem.title"></v-list-item-title>
           </v-list-item-content>
         </v-list-item>
+      </v-list-group>
+    </v-list>
 	</v-list>	
 </template>
 
@@ -24,9 +55,43 @@
 		data() {
 			return {
 			items: [
-          { title: 'Dashboard', icon: 'mdi-view-dashboard' },
-          { title: 'Photos', icon: 'mdi-image' },
-          { title: 'About', icon: 'mdi-help-box' },
+          
+					{
+						title: '수강하기',
+						icon : 'mdi-school',
+						subItems: [
+							{
+								title: '주간정신전력교육',
+								//to: '/'
+							},
+							{
+								title: '데일리 역사 상식',
+								//to: '/'
+							},
+							{
+								title: '컨텐츠3',
+								//to: '/'
+							}
+						]
+					},
+          { 
+						title: '도움말', 
+						icon: 'mdi-help-box',
+						subItems: [
+							{
+								title: '가이드',
+								//to: '/'
+							},
+							{
+								title: 'FAQ',
+								//to: '/'
+							},
+							{
+								title: '기획의도',
+								//to: '/'
+							}
+						]
+					},
         ]
 			}
 		}
