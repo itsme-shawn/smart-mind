2507749bedbb857803a8a3cc95c3faf93658b354
Choieeh <chth0182@naver.com>
2020-10-06 14:54:44 +0000

    mainpage

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,27 +2,27 @@
   <v-container>
     <v-row class="text-center">
       <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
+      <v-img
+        contain
+        :src="require('../assets/soldier.svg')"
+        max-height="300"
+        max-width="2000"
+      >
+      </v-img>
+        
       </v-col>
 
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
+          정신차렷!
         </h1>
 
         <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a
-            href="https://community.vuetifyjs.com"
-            target="_blank"
-          >Discord Community</a>
+          정신차렷!은 언제, 어디서나 정신전력 컨텐츠를 즐길 수 있는 플랫폼입니다.
+          
         </p>
+        <p>
+        <button>시작하기</button></p>
       </v-col>
 
       <v-col
