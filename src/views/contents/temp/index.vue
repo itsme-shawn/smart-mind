145095f0bd4695c4c46d912e1b113cdaf89e41cc
HyeonSoo Choi <chs98105@gmail.com>
2020-10-07 11:19:32 +0000

    axios 로 데이터가져오기 성공 + 에러페이지 생성

--- src/views/contents/temp/index.vue
+++ src/views/contents/temp/index.vue
@@ -1,9 +1,13 @@
 <template>
-	<div class="text-center" style="font-size:50px">
-		임시 - storage 연습용
-
+	<div class="text-center" >
+		임시 - storage 연습용<br>
+    로그인 상태에서만 write 가능<br>
+    read 하면 write 한 파일 화면에 렌더링
 		<v-sheet>
+
+        <v-textarea v-model="text"></v-textarea>
 		<v-btn @click="write">write</v-btn>
+    <v-btn @click="read">read</v-btn>
 		</v-sheet>
 
 	</div>
@@ -13,17 +17,26 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
 	data() {
 		return {
-			text : ''
+      text : '',
+      url : ''
 		}
 	},
-	methods: { // gc 로 corn 해야함
+	methods: {
 		async write() {
 			const sn = await this.$firebase.storage().ref().child('xxx.test').putString('abcd')
-			console.log(sn)
-			}
+      const url = await sn.ref.getDownloadURL()
+      this.url = url
+      },
+    async read() {
+      const r = await axios.get(this.url)
+      console.log(r)
+      this.text = r.data
+    }
   }
 }
 </script>
