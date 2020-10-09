6eedf7a9c680d707039a43de382d216e7afde083
Now Kim <peterhyunjae@naver.com>
2020-10-10 00:22:57 +0900

    mypage done!

--- src/views/site/menu.vue
+++ src/views/site/menu.vue
@@ -11,16 +11,17 @@
 		</v-list-item>
 
 		<v-divider></v-divider>
-	
+
 		<v-list>
-			
+
 			<v-list-item :to="pathHome">
         <v-list-item-icon>
           <v-icon>mdi-home</v-icon>
         </v-list-item-icon>
-      	<v-list-item-title>홈</v-list-item-title>
+
+ <v-list-item-title>홈</v-list-item-title>
       </v-list-item>
-			
+
       <v-list-group
         v-for="(item, i) in items"
         :key="i"
@@ -46,82 +47,80 @@
         </v-list-item>
       </v-list-group>
     </v-list>
-	</v-list>	
+	</v-list>
 </template>
 
-
-
 <script>
-	export default {
-		data() {
-			return {
-			pathHome : '/',
+export default {
+	data () {
+		return {
+			pathHome: '/',
 			items: [
-          
-					{
-						title: '수강하기',
-						icon : 'mdi-school',
-						to : '/',
-						subItems: [
-							{
-								title: '대시보드',
-								to: '/dashboard'
-							},
-							{
-								title: '주간정신전력교육',
-								to: '/jungsin'
-							},
-							{
-								title: '이달의 정신전력',
-								to: '/daily_history'
-							},
-							{
-								title: '우리 역사 바로알기',
-								to: '/temp'
-							}
-						]
-					},
-					{
-						title: '마이페이지',
-						icon : 'mdi-account',
-						to : '/',
-						subItems: [
-							{
-								title: '개인정보수정',
-								to: '/account'
-							},
-							{
-								title: '정신전력 현황',
-								to: '/condition'
-							},
-							{
-								title: '부대관리',
-								to: '/budae'
-							}
-							
-						]
-					},
-          { 
-						title: '도움말', 
-						icon: 'mdi-help-box',
-						subItems: [
-							{
-								title: '가이드',
-								to: '/guide'
-							},
-							{
-								title: 'FAQ',
-								to: '/FAQ'
-							},
-							{
-								title: '기획의도',
-								to: '/intentions'
-							}
-						]
-					},
-        ]
-			}
+
+				{
+					title: '수강하기',
+					icon: 'mdi-school',
+					to: '/',
+					subItems: [
+						{
+							title: '대시보드',
+							to: '/dashboard'
+						},
+						{
+							title: '주간정신전력교육',
+							to: '/jungsin'
+						},
+						{
+							title: '이달의 정신전력',
+							to: '/daily_history'
+						},
+						{
+							title: '우리 역사 바로알기',
+							to: '/temp'
+						}
+					]
+				},
+				{
+					title: '마이페이지',
+					icon: 'mdi-account',
+					to: '/',
+					subItems: [
+						{
+							title: '개인정보수정',
+							to: '/account'
+						},
+						{
+							title: '정신전력 현황',
+							to: '/condition'
+						},
+						{
+							title: '부대관리',
+							to: '/budae'
+						}
+
+					]
+				},
+				{
+					title: '도움말',
+					icon: 'mdi-help-box',
+					subItems: [
+						{
+							title: '가이드',
+							to: '/guide'
+						},
+						{
+							title: 'FAQ',
+							to: '/FAQ'
+						},
+						{
+							title: '기획의도',
+							to: '/intentions'
+						}
+					]
+				}
+			]
 		}
-		
 	}
+
+}
 </script>
