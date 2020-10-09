7ef09dadc2d098bc228ad57ebf53340182c2f3b6
Choieeh <chth0182@naver.com>
2020-10-09 14:55:47 +0000

    mainpage 그리드, 크기에 따른 변화 구현

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,46 +1,50 @@
 <template>
   <v-container class="text-center">
     <v-layout column>
-      <v-flex xs12 md6>
-        <v-col cols="12" align="center">
-          <v-img
-            contain
-            :src="require('../assets/soldier.svg')"
-            min-height="100"
-            max-width="1000"
-          >
-          </v-img>
-        </v-col>
-      </v-flex>
-      <v-flex xs12 md6>
-        <v-col class="mb-4" cols="12">
-          <h1 class="display-2 font-weight-bold mb-3">정신차렷!</h1>
-
-          <p class="subheading font-weight-regular">
-            정신차렷!은 언제, 어디서나 정신전력 컨텐츠를 즐길 수 있는
-            플랫폼입니다.
-          </p>
-          <p>
-            <v-btn
-              color="cyan lighten-4"
-              elevation="8"
-              medium
-              @click="dashboard"
-              >시작하기</v-btn
+      <v-layout row wrap :style="myContainer">
+        <v-flex xs12 md6 lg6 class="order-lg-2">
+          <v-col align="center">
+            <v-img
+              contain
+              :src="require('../assets/soldier.svg')"
+              min-height="100"
+              max-width="1000"
             >
-          </p>
-        </v-col>
-      </v-flex>
+            </v-img>
+          </v-col>
+        </v-flex>
+        <v-layout align-center justify-center>
+          <v-flex xs12 md6 lg6 class="order-lg-1">
+            <v-col>
+              <h1 class="display-2 font-weight-bold mb-3">정신차렷!</h1>
+
+              <p class="subheading font-weight-regular">
+                정신차렷!은 언제, 어디서나 정신전력 컨텐츠를 즐길 수 있는
+                플랫폼입니다.
+              </p>
+              <p>
+                <v-btn
+                  color="cyan lighten-4"
+                  elevation="8"
+                  medium
+                  @click="dashboard"
+                  >시작하기</v-btn
+                >
+              </p>
+            </v-col>
+          </v-flex>
+        </v-layout>
+      </v-layout>
       <br />
       <v-flex xs12 md6>
-        <v-row class="text-center" :style="myContainer">
-          <v-layout row>
+        <v-row class="text-center">
+          <v-layout row align-center>
             <v-flex xs6>
               <v-sheet class="mb-5">
                 <v-img
                   contain
                   :src="require('../assets/you.jpg')"
-                  max-width="700"
+                  max-width="600"
                 ></v-img>
               </v-sheet>
             </v-flex>
@@ -74,57 +78,37 @@
         </v-row>
       </v-flex>
       <v-flex xs12 md12>
-        <h2 class="headline font-weight-bold mb-3"><br />이달의 정신전력</h2>
-        <v-row no-gutters>
-          <v-col cols="4">
-            <v-card class="pa-2" outlined tile>
-              best1<br />Since 9 + 4 = 13 &gt; 12, this 4-column-wide div gets
-              wrapped onto a new line as one contiguous unit.
-            </v-card>
-          </v-col>
-          <v-col cols="6" rows="5">
-            <v-card class="pa-2" outlined tile>
-              .col-6<br />Subsequent columns continue along the new line.
-            </v-card>
-          </v-col>
-          <v-col cols="6">
-            <v-card class="pa-2" outlined tile>
-              .col-6<br />Subsequent columns continue along the new line.
-            </v-card>
-          </v-col>
-        </v-row>
+        <v-card class="mx-auto" max-width="500">
+                  
+
+          <v-container fluid>
+            <v-row dense>
+              <v-col v-for="card in cards" :key="card.title" :cols="card.flex">
+                <v-card>
+                  <v-img
+                    :src="card.src"
+                    class="white--text align-end"
+                    gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                    height="200px"
+                  >
+                    <v-card-title v-text="card.title"></v-card-title>
+                  </v-img>
+
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+
+                    <v-btn icon>
+                      <v-icon>mdi-heart</v-icon>
+                    </v-btn>
+
+                    
+                  </v-card-actions>
+                </v-card>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card>
       </v-flex>
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3"></h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3"></h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
-      </v-col>
     </v-layout>
   </v-container>
 </template>
@@ -141,7 +125,7 @@ export default {
   data() {
     return {
       myContainer: {
-        Width: "100%",
+        backgroundColor: "grey",
       },
     };
   },
