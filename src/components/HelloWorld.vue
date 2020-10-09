7d3e363ecfb2e717f7eadcf88ead5e875111b366
Choieeh <chth0182@naver.com>
2020-10-09 06:58:55 +0000

    mainpage

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,73 +2,52 @@
   <v-container>
     <v-row class="text-center">
       <v-col cols="12">
-      <v-img
-        contain
-        :src="require('../assets/soldier.svg')"
-        min-height="100"
-        max-width="1000"
-      >
-      </v-img>
-        
+        <v-img
+          contain
+          :src="require('../assets/soldier.svg')"
+          min-height="100"
+          max-width="1000"
+        >
+        </v-img>
       </v-col>
 
       <v-col class="mb-4" cols="12">
-        <h1 class="display-2 font-weight-bold mb-3">
-          정신차렷!
-        </h1>
+        <h1 class="display-2 font-weight-bold mb-3">정신차렷!</h1>
 
         <p class="subheading font-weight-regular">
-          정신차렷!은 언제, 어디서나 정신전력 컨텐츠를 즐길 수 있는 플랫폼입니다.
-          
+          정신차렷!은 언제, 어디서나 정신전력 컨텐츠를 즐길 수 있는
+          플랫폼입니다.
         </p>
         <p>
-        <v-btn
-        color="primary"
-        elevation="8"
-        medium
-        @click="dashboard"
-        >시작하기</v-btn></p>
-      </v-col>
-      
-      
-      <v-row class="text-center">
-        <v-col class="mb-5" cols="12">
-        <v-img
-        contain
-        :src="require('../assets/you.jpg')"
-        max-width="500"
-        ></v-img>
-        </v-col>
-      
-        <v-col>
-        <h2 class="headline font-weight-bold mb-3">
-          주간 정신전력 교육
-        </h2>
-        <p class="subheading font-weight-regular">
-          유관순 blahblah~~
-        </p>
-        </v-col>
-      </v-row>
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
+          <v-btn color="cyan lighten-4" elevation="8" medium @click="dashboard"
+            >시작하기</v-btn
           >
-            {{ next.text }}
-          </a>
-        </v-row>
-      
+        </p>
+      </v-col>
 
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          
-        </h2>
+      <v-row class="text-center" :style="myContainer">
+        <v-layout row>
+          <v-flex>
+            <v-sheet class="mb-5">
+              <v-img
+                contain
+                :src="require('../assets/you.jpg')"
+                max-width="500"
+              ></v-img>
+            </v-sheet>
+          </v-flex>
+          <v-flex>
+            <h2 class="headline font-weight-bold mb-3">주간 정신전력 교육</h2>
+            <p class="subheading font-weight-regular">유관순 blahblah~~</p>
+          </v-flex>
+        </v-layout>
+      </v-row>
+      <v-row justify="center">
+         <enlargeable-image :src="require('../assets/you.jpg')" :src_large="require('../assets/you.jpg')" />
+      </v-row>
+
+      <v-col class="mb-5" cols="12">
+        <h2 class="headline font-weight-bold mb-3"></h2>
 
         <v-row justify="center">
           <a
@@ -83,13 +62,8 @@
         </v-row>
       </v-col>
 
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          
-        </h2>
+      <v-col class="mb-5" cols="12">
+        <h2 class="headline font-weight-bold mb-3"></h2>
 
         <v-row justify="center">
           <a
@@ -109,12 +83,19 @@
 
 <script>
 export default {
-	name: 'HelloWorld',
-  
+  name: "HelloWorld",
+
   methods: {
-    dashboard:function(event){
-      this.$router.push('/dashboard')
+    dashboard: function (event) {
+      this.$router.push("/dashboard");
+    },
+  },
+  data() {
+    return{
+      myContainer:{
+        Width:'100%'
+      }
     }
   }
-}
+};
 </script>
