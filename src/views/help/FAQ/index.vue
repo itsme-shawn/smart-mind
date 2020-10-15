06c9897193e2cdd2aafc4927dbd94d9aca811c71
Now Kim <peterhyunjae@naver.com>
2020-10-15 23:15:51 +0900

    card 형식으로 한 줄은 토스처럼 만들었는데, list형식으로 바꾸고 key값을 부여해서 누르는 거에 따라 그것만 열리게 해야함 지금은 다 열리고 닫힘

--- src/views/help/FAQ/index.vue
+++ src/views/help/FAQ/index.vue
@@ -51,7 +51,7 @@
 						color="grey"
 						class="mr-4"
 						></v-avatar>
-						<strong class="title">Title {{ n }}</strong>
+						<strong class="title">질문 {{ n }}</strong>
 					</v-row>
 
 					<p>
@@ -74,13 +74,50 @@
 
 		<!--질문 끝-->
 
+	<!--질문 시작-->
+		<template>
+			<v-hover v-slot:default="{ hover }">
+				<v-card
+					:elevation="hover ? 16 : 2"
+					:class="{ 'on-hover': hover }"
+					class="mx-auto"
+					width="600"
+				>
+					<v-card-actions @click="show = !show">
+						<v-card-title>
+						로그인이 안 돼요.
+						</v-card-title>
+						<v-spacer></v-spacer>
+					<v-btn
+						icon
+						@click="show = !show"
+					>
+						<v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+					</v-btn>
+					</v-card-actions>
+
+					<v-expand-transition>
+					<div v-show="show">
+						<v-divider></v-divider>
+						<v-card-text>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+						</v-card-text>
+					</div>
+					</v-expand-transition>
+				</v-card>
+			</v-hover>
+		</template>
+	<!--질문 끝-->
+	
+
     </v-container>
 </template>
 <script>
 export default {
 	data: () => ({
 		length: 3,
-		window: 0
+		window: 0,
+		show: false
 	})
 }
 </script>
