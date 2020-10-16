5f6592982955eb78ab8c3532af18d3836e8ae29a
Choieeh <chth0182@naver.com>
2020-10-16 14:23:53 +0000

    mainpage 이미지추가

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -137,18 +137,19 @@
         class="fullpage-container"
         :height="height"
         dark
-        :src="require('../assets/main3-1.jpg')"
+        :src="require('../assets/main5-2.jpg')"
       >
-        <v-row align="center" justify="end">
-          <div class="text-center order-lg-2" xs12 sm12 md12 lg6>
+      <v-row align="center" justify="center" height="700">
+      
+        <v-row align="center" justify="center" class="order-md-2 order-lg-2">
+          <v-flex class="text-center " xs12 sm12 md6 lg6>
             <h1 class="display-1 font-weight-bold mb-4 custom1">
               주간정신전력
             </h1>
-            <h4 class="subheading custom2">
-              이번주의 정신전력을 간단하게 확인하고 평가할 수 있습니다!
-            </h4>
-            <br />
-            <div align="center">
+            <h2 class="subheading custom2">
+              이번주의 정신전력을 간단하게 확인하고 <br>평가할 수 있습니다!
+            </h2>
+            <!-- <div align="center">
               <v-btn
                 class="font-weight-bold"
                 color="white"
@@ -160,11 +161,44 @@
                 >확인하기</v-btn
               >
               
-            </div>
-          </div>
+            </div> -->
+          </v-flex>
         </v-row>
-      </v-parallax>
+        <v-flex xs12 sm12 md6 lg6 class="order-md-1 order-lg-1">
+      <v-hover>
+			<template v-slot:default="{ hover }">
+				<v-card class="mx-auto mt-5" max-width="800">
+				<v-responsive :aspect-ratio="16/9">
+				<!--해당 컨텐츠에 맞게 이미지를 넣어야 함-->
+					<v-img class="white--text" max-height="500"
+					src="https://cdn.vuetifyjs.com/images/cards/docks.jpg">
+					<v-card-title>이번주의 정신전력 교육</v-card-title>
+					</v-img>
+				<!--해당 컨텐츠에 맞게 이미지를 넣어야 함-->
 
+					<!--이번이 몇 주차인지 알 수 있게 하기 위해 변동되어야 하는 부분-->
+					<v-card-subtitle class="pb-2">
+					10월 2주차
+					</v-card-subtitle>
+					<!--이번이 몇 주차인지 알 수 있게 하기 위해 변동되어야 하는 부분-->
+					<v-card-text class="text--primary">
+					<h3>믿음직하고 든든한 국군, ‘대체불가’ 특수전부대</h3>
+
+					<div>김 관 용 이데일리 정치부 외교안보팀장</div>
+					</v-card-text>
+					<v-fade-transition>
+					<v-overlay v-if="hover" absolute color="#dedede">
+						<v-btn>참여하기</v-btn>
+					</v-overlay>
+					</v-fade-transition>
+					</v-responsive>
+				</v-card>
+			</template>
+		</v-hover>
+      </v-flex>
+      </v-row>
+      </v-parallax>
+    
       <v-flex xs12 md6>
         <h2 class="headline font-weight-bold mb-3"><br />우리 역사 바로알기</h2>
         <v-row>
