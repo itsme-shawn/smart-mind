9f715c58f3fa8e19582a174968dfdd5663ee6a44
HyeonSoo Choi <chs98105@gmail.com>
2020-10-16 10:48:12 +0000

    appbar 의 title 누를 시 홈 가게끔 하기

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,7 @@
 			<v-btn @click="readOnce">read once test</v-btn>
 			-->
       <router-view />
-      
+
     </v-main>
 	<site-footer :footer="site.myFooter" ></site-footer>
   </v-app>
@@ -33,59 +33,26 @@
 
 <script>
 // vue 에서는 src를 @로 표시
-import SiteTitle from "@/views/site/title";
-import SiteFooter from "@/views/site/footer";
-import SiteMenu from "@/views/site/menu";
-import SiteAuth from "@/views/site/auth";
+import SiteTitle from '@/views/site/title'
+import SiteFooter from '@/views/site/footer'
+import SiteMenu from '@/views/site/menu'
+import SiteAuth from '@/views/site/auth'
 
 export default {
-  components: { SiteTitle, SiteFooter, SiteMenu, SiteAuth },
-  name: "App",
-  data() {
-    return {
-      site: {
-        myTitle: "정신차렷!",
-        myFooter: "Copyright by Came To Win",
-        menu: [],
-      },
-      right: null,
-      drawer: false,
-      fontcolor: "black",
-      solid: "1px",
-    };
-  },
-  mounted() {
-    console.log(this.$firebase);
-  },
-  methods: {
-    write() {
-      console.log("write");
-      this.$firebase.database().ref().child("abcd").set({
-        title: "abc",
-        text: "ttt",
-      });
-    },
-    read() {
-      // database 를 리스닝하고 있다가 database 가 바뀔 때마다 자동으로 실행
-      console.log("read");
-      // sn : snapshot
-      this.$firebase
-        .database()
-        .ref()
-        .child("abcd")
-        .on("value", (sn) => {
-          console.log(sn.val());
-        });
-    },
-    async readOnce() {
-      // database 에 일회성(once) 로 data 받아옴 (변할때 즉각적으로 반영 x)
-      const sn = await this.$firebase
-        .database()
-        .ref()
-        .child("abcd")
-        .once("value");
-      console.log(sn.val());
-    },
-  },
-};
+	components: { SiteTitle, SiteFooter, SiteMenu, SiteAuth },
+	name: 'App',
+	data () {
+		return {
+			site: {
+				myTitle: '정신차렷!',
+				myFooter: 'Copyright by Came To Win',
+				menu: []
+			},
+			right: null,
+			drawer: false,
+			fontcolor: 'black',
+			solid: '1px'
+		}
+	}
+}
 </script>
