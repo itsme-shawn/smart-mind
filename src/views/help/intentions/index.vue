11dccba0c15d70996655cd17df16eac1e8ae2b5e
nowkim <peterhyunjae@naver.com>
2020-10-18 00:29:51 +0900

    기획의도 페이지 기본 틀 구성

--- src/views/help/intentions/index.vue
+++ src/views/help/intentions/index.vue
@@ -1,6 +1,42 @@
 <template>
-	<div class="text-center" style="font-size:50px">
-		기획 의도
-	</div>
-	
+	<v-container>
+        <div>
+            <v-card-text>
+                <v-toolbar-title class="font-weight-bold mt-5">기획의도</v-toolbar-title>
+            </v-card-text>
+        </div>
+		<v-divider></v-divider>
+		
+	<v-sheet class="pt-10">
+		<h1>정신전력 교육을 쉽고 간편하게.</h1><br>
+		<h3>정신차렷은 불편하고 번거로운 정신전력교육을 개선하기 위해 만들어졌습니다.</h3><br>
+		<h3>어렵고 불편한 사용법 대신 직관적이고 간편한 사용을 위해 노력했습니다.</h3><br>
+		<h3>쉽게 참여하고, 쉽게 관리/감독을 시작하세요.</h3><br>
+
+		<v-divider></v-divider>
+	</v-sheet>
+
+	<v-sheet class="pt-10 text-center">
+		<h1>어떠한 순간에도, 정신차렷으로.</h1><br>
+		<h3>일과시간 특수한 보직으로 참여할 수 없는 병사들도,</h3><br>
+		<h3>휴가 복귀로 격리되어 참여할 수 없는 병사들도,</h3><br>
+		<h3>근무나 당직으로 참여할 수 없는 병사들도,</h3><br>
+		<span class="green--text"><h1>원하는 시간에, 어디에서나</h1><br></span>
+
+		<v-divider></v-divider>
+	</v-sheet>
+	<!--위의 경우를 좀 더 디테일하게 세분화해서 표현-->
+
+	<v-sheet class="pt-10">
+		<h1>쉽고 빠른 관리.</h1><br>
+		<h3>지나간 정신전력을 다시 보고 싶을 때,</h3><br>
+		<h3>병사들의 참여율과 작성 내용을 한눈에 정리해주는 도구들,</h3><br>
+		<h3>다른 병사들의 생각을 공유하며, <strong class="green--text text--lighten-1">함께 성장하기</strong></h3><br>
+		<v-divider></v-divider>
+	</v-sheet>
+
+	<!--캡처해서 이미지 넣기-->
+		
+
+    </v-container>
 </template>
