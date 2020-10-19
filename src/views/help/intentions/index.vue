5d5398fa5415abdf5d7a05c1bbf699fd21263f3f
nowkim <peterhyunjae@naver.com>
2020-10-19 22:43:21 +0900

    기획의도 - 임시 사진 첨부

--- src/views/help/intentions/index.vue
+++ src/views/help/intentions/index.vue
@@ -76,21 +76,18 @@
 		<v-carousel-item
 		v-for="(slide, i) in slides"
 		:key="i"
+		:src="slide.src"
 		>
-		<v-sheet
-			:color="colors[i]"
-			height="100%"
-		>
+		
 			<v-row
 			class="fill-height"
 			align="center"
 			justify="center"
 			>
-			<div class="display-3">
+			<!-- <div class="display-3">
 				{{ slide }} Slide
-			</div>
+			</div> -->
 			</v-row>
-		</v-sheet>
 		</v-carousel-item>
 	</v-carousel>
 	</template>
@@ -124,20 +121,21 @@
 	  ],
 	  
 	  // carousel data
-	  colors: [
-          'indigo',
-          'warning',
-          'pink darken-2',
-          'red lighten-1',
-          'deep-purple accent-4',
+		slides: [
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+          },
         ],
-        slides: [
-          '캡처해서 배경 colors 대신에 캡처 사진 url 넣으면 끝 / First',
-          '캡처해서 배경 colors 대신에 캡처 사진 url 넣으면 끝 / Second',
-          '캡처해서 배경 colors 대신에 캡처 사진 url 넣으면 끝 / Third',
-          '캡처해서 배경 colors 대신에 캡처 사진 url 넣으면 끝 / Fourth',
-          '캡처해서 배경 colors 대신에 캡처 사진 url 넣으면 끝 / Fifth',
-        ]
+        
     }),
   }
 </script>
