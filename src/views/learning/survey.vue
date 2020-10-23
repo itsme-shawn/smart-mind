1d703f968e63d89f0d3f9a57022e9f5b6e9bb5eb
HyeonSoo Choi <chs98105@gmail.com>
2020-10-23 13:15:58 +0000

    survey.vue 제출버튼 색상변경

--- src/views/learning/survey.vue
+++ src/views/learning/survey.vue
@@ -1,19 +1,19 @@
-<!-- 진짜 코드
-  @/components/display-content.vue 에서 사용하는 컴포넌트
+<!--
+  @/components/display-content.vue 의 자식 컴포넌트
 -->
 
 <template>
   <v-container class="mt-5 mx-auto pa-5">
     <v-divider/>
     <br/>
-    <h2 style="color:green">의견 제출</h2>
-    <h5>자신의 생각을 작성해보세요</h5>
+    <h2 style="color:rgb(21, 179, 137)">의견 제출</h2>
+    <h5 style="color:rgb(21, 179, 137)">자신의 생각을 작성해보세요</h5>
     <br/>
     <h3>1. {{q1}}</h3>
     <v-textarea v-model="a1" class="mt-3" auto-grow solo clearable clear-icon="mdi-close-circle" label="작성해주세요" ></v-textarea>
     <h3>2. {{q2}}</h3>
     <v-textarea v-model="a2" class="mt-3" auto-grow solo clearable clear-icon="mdi-close-circle" label="작성해주세요" ></v-textarea>
-    <v-btn @click="submitOwn" color="primary" large>제출</v-btn>
+    <v-btn  @click="submitOwn" dark rounded color="rgb(21, 179, 137)" large>제출</v-btn>
 
   </v-container>
 
