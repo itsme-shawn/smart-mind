9e7782ec94b30cbb8a4495e5d4b146f7cb90acfb
HyeonSoo Choi <chs98105@gmail.com>
2020-10-31 14:52:52 +0000

    eslint

--- src/views/help/intentions/index.vue
+++ src/views/help/intentions/index.vue
@@ -55,7 +55,6 @@
 	</v-sheet>
 	<!--위의 경우를 좀 더 디테일하게 세분화해서 표현-->
 
-
 	<v-sheet class="pt-10">
 		<h1>쉽고 빠른 관리.</h1><br>
 		<h3>지나간 정신전력을 다시 보고 싶을 때,</h3><br>
@@ -63,7 +62,6 @@
 		<h3>다른 병사들의 생각을 공유하며, <strong class="green--text text--lighten-1">함께 성장하기</strong></h3><br>
 		<v-divider></v-divider>
 	</v-sheet>
-
 
 	<!--캡처해서 colors 대신에 캡처 사진 url 넣으면 끝-->
   <!--
@@ -94,51 +92,50 @@
 	</template>
   -->
 
-
     </v-container>
 </template>
 <script>
-  export default {
-    data: () => ({
+export default {
+	data: () => ({
 		// 타임라인 데이터
-      items: [
-        {
-          color: 'red lighten-2',
+		items: [
+			{
+				color: 'red lighten-2',
 		  icon: 'mdi-star',
 		  title: '특수한 업무나 보직으로 참여할 수 없는 병사들',
 		  content: '일과를 특수한 보직으로 인해 다른 병사들과 함께 하지 않는 병사의 경우 정신전력 교육에 참여하지 못하는 일이 일어나곤 합니다. px병이나, 운행이 있는 운전병의 경우가 대표적인 경우입니다. 우리는 단 몇 분만 투자하면 참여할 수 있지만, 그럴 수 없는 현실을 보았습니다. '
-        },
-        {
-          color: 'purple darken-1',
+			},
+			{
+				color: 'purple darken-1',
 		  icon: 'mdi-book-variant',
 		  title: '격리로 인해 참여할 수 없는 병사들',
 		  content: '코로나19로 인해 휴가 후 격리된 생활을 하는 병사들이 있습니다. 다른 일과는 격리되어서 할 수 없지만 비대면으로 훈련을 진행한다면 격리되어도 다른 병사들과 함께할 수 있지 않을까? 하는 생각을 하게 되었고, 이제는 정신전력교육을 비대면으로 실시하게 되어 격리된 병사에게도 책임감과 소속감을 부여합니다.'
-        },
-        {
-          color: 'green lighten-1',
+			},
+			{
+				color: 'green lighten-1',
 		  icon: 'mdi-airballoon',
 		  title: '근무나 당직으로 참여할 수 없는 병사들',
 		  content: '근무나 당직이 있을 때 장소와 시간에 구애받기 때문에 정신전력교육에 참여할 수 없습니다. 정신차렷과 함께라면 언제, 어디서나 가능한 시간대에 정신전력 교육에 참여할 수 있습니다.'
-        }
+			}
 	  ],
 
 	  // carousel data
 	  colors: [
-          'indigo',
-          'warning',
-          'pink darken-2',
-          'red lighten-1',
-          'deep-purple accent-4',
+			'indigo',
+			'warning',
+			'pink darken-2',
+			'red lighten-1',
+			'deep-purple accent-4'
 		],
 
-        slides: [
+		slides: [
 		  // require('../../../assets/help_intention1.png'),
 		  // require('../../../assets/help_intention2.png'),
 		  // require('../../../assets/help_intention3.png'),
 		  // require('../../../assets/help_intention4.png'),
 		  // require('../../../assets/help_intention5.png'),
 
-        ]
-    }),
-  }
+		]
+	})
+}
 </script>
